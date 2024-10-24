df38cf604fffd59e67b7d497b09988ca954e8a20
PedroPort3s <pedrodportes@hotmail.com>
2024-10-23 22:17:34 -0300

    feat: creates models project

---
+++ Models/Truck.cs
@@ -0,0 +1,11 @@
+﻿namespace Models
+{
+    public sealed class Truck
+    {
+        public string Modelo { get; set; }
+        public int AnoFabricacao { get; set; }
+        public string CodigoChassi { get; set; }
+        public string Cor { get; set; }
+        public string Planta { get; set; }
+    }
+}
